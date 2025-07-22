3500dd35c2da36b3d187d4b86b16040544c9bf40
Deyanvkc <deyan.vukic@epitech.eu>
2025-07-22 10:07:56 +0200

    change var name

--- fight_simulator/fight_simulator.py
+++ fight_simulator/fight_simulator.py
@@ -1,13 +1,13 @@
 import json, sys, os
 from random import randint, choice
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from fighter_generator.fighter_gen import FighterGen
+from fighter_generator.fighter_gen import fighter_gen
 
 class fight_simulator:
     
   def generate_fighters(self):
-    FighterGen().generate("player.json","fs2/fighters")
-    FighterGen().generate("enemy.json","fs2/fighters")
+    fighter_gen().generate("player.json","fs2/fighters")
+    fighter_gen().generate("enemy.json","fs2/fighters")
     
 simulator = fight_simulator()
 simulator.generate_fighters()
